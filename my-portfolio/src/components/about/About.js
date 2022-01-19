02035b4467874abf9adac48d892359d3956909fa
shoma <shomasdutt@gmail.com>
2022-01-19 15:10:57 +0000

    landing and about

--- my-portfolio/src/components/about/About.js
+++ my-portfolio/src/components/about/About.js
@@ -7,7 +7,13 @@ const About = () => {
       <p className="sectionTitle" id="about">
         About Me
       </p>
-      <p id="aboutText">My name is Dee, and I am a yoga teacher.</p>
+      <p id="aboutText">
+        My name is Dee, and I teach yoga. Join me and improve your life!{" "}
+        <br></br>
+        Finding peace and contentment in the present moment is one of the most
+        challenging things to do, and yoga provides the tools we need to find
+        that much-desired stillness.
+      </p>
     </section>
   );
 };
