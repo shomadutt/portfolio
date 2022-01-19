b5880d2e875cadb29127f5620155c584af96bc2d
shoma <shomasdutt@gmail.com>
2022-01-19 18:56:32 +0000

    Using partials

--- my-portfolio/src/components/testimonial/Testimonial.js
+++ my-portfolio/src/components/testimonial/Testimonial.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import './testimonial.scss';
+import React from "react";
+import "./testimonial.scss";
 
 const Testimonial = () => {
-  return (  
-  <section>
-  <p className="sectionTitle" id="testimonial">
-    Testimonials
-  </p>
-  <p id="textPara">
-    My name is Dee, and I teach yoga. Join me and improve your life!{" "}
-    <br></br>
-    Finding peace and contentment in the present moment is one of the most
-    challenging things to do, and yoga provides the tools we need to find
-    that much-desired stillness.
-  </p>
-</section>
+  return (
+    <section>
+      <p className="sectionTitle" id="testimonial">
+        Testimonials
+      </p>
+      <p id="textPara">
+        Carol
+        <br></br>
+        <br></br>
+        Yoga For Life has been my saving grace! I can not say enough about Dee,
+        and her knowledge of the practice but also of kinaesthetics. Yoga has
+        brought me physical health, but more importantly peace and an over all
+        feeling of well being! I feel fortunate to practice at such a welcoming
+        studio and will be forever grateful for the knowledge I have gained!
+        <br></br>
+        <br></br>
+        Becky
+        <br></br>
+        <br></br>
+        The first time I walked into the Yoga For Life studio I felt so at home.
+        Dee was amazing, she made me feel like I was the most important student
+        in the class. She encouraged me to try new poses and encouraged me to
+        reach the next level. Every time I finish a class I feel taller and
+        stronger and I thank my body for what it can do. Thanks to Dee at Yoga
+        For Life!!!
+      </p>
+    </section>
   );
 };
 
