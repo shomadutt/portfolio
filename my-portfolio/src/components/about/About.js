bb235c2f5ac93e62fb1faca990a598ed1cb74896
shoma <shomasdutt@gmail.com>
2022-01-25 11:45:15 +0000

    Added text

--- my-portfolio/src/components/about/About.js
+++ my-portfolio/src/components/about/About.js
@@ -4,20 +4,52 @@ import "./about.scss";
 const About = () => {
   return (
     <div className="container-fluid px-0">
-    <section id="aboutSection">
-     
-      <p className="sectionTitle" id="about">
-        About Me
-      </p>
-      <p id="textPara">
-        My name is Dee, and I teach yoga. Join me and improve your life!{" "}
-        <br></br>
-        Finding peace and contentment in the present moment is one of the most
-        challenging things to do, and yoga provides the tools we need to find
-        that much-desired stillness.
-      </p>
-     
-    </section>
+      <section id="aboutSection">
+        <p className="sectionTitle" id="about">
+          About Me
+        </p>
+        <p id="textPara">
+          My name is Dee, and I teach yoga. Join me and improve your life!{" "}
+          <br></br><br></br>
+          Finding peace and contentment in the present moment is one of the most
+          challenging things to do, and yoga provides the tools we need to find
+          that much-desired stillness. 
+          <br></br><br></br>
+          Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Donec eget diam ut ipsum porta dignissim in non ex.
+          Nam aliquet, arcu sit amet pharetra tincidunt, mi turpis dignissim
+          lorem, ac commodo mauris ante at arcu. Mauris mattis risus sem, non
+          finibus turpis iaculis rhoncus. Vivamus porta efficitur mi in
+          consequat. Class aptent taciti sociosqu ad litora torquent per conubia
+          nostra, per inceptos himenaeos. Proin nec enim purus. Vestibulum
+          pellentesque semper erat, vel vulputate ex pretium a. Aliquam varius
+          orci vitae neque pulvinar, id interdum metus dictum. Etiam tempor,
+          lorem sed sollicitudin imperdiet, felis diam ultrices lacus, a
+          consequat risus sem vulputate massa. Maecenas dictum, augue ut euismod
+          vulputate, velit mauris pharetra dui, sit amet vestibulum leo risus a
+          turpis.
+          <br></br><br></br>
+          Aliquam et risus diam. Curabitur eget ullamcorper dolor. Pellentesque
+          condimentum dapibus auctor. Nunc non scelerisque metus. Fusce rutrum
+          leo sapien, pellentesque sodales purus tempor id. Pellentesque
+          dignissim eu odio in consectetur. Ut faucibus in massa in consequat.
+          Nam vitae gravida ipsum. Aliquam at ex elit. Morbi in auctor sem, sit
+          amet elementum quam. In hac habitasse platea dictumst. Phasellus sed
+          dui eu eros gravida molestie a at felis.
+          <br></br><br></br>
+          Nulla vitae quam et metus ultricies volutpat pretium vitae lorem.
+          Curabitur at congue odio. Fusce arcu est, congue a elit ut, congue
+          convallis erat. Sed quis purus libero. Praesent nunc tellus, venenatis
+          id lacus quis, commodo posuere nisi. Maecenas pharetra blandit neque,
+          in consequat neque auctor eget. Curabitur suscipit, lectus lobortis
+          auctor scelerisque, mauris mi vulputate sem, id facilisis ligula sem
+          consectetur erat. Praesent congue sit amet nulla quis porta. Phasellus
+          non mi et tortor convallis rhoncus sed sit amet urna. Donec lorem sem,
+          tincidunt vel congue vel, sodales ut urna. Maecenas efficitur magna
+          quis risus ultricies, ac placerat diam pulvinar. Sed magna mi, pretium
+          eu risus id, sollicitudin interdum enim.
+        </p>
+      </section>
     </div>
   );
 };
--- my-portfolio/src/components/testimonial/Testimonial.js
+++ my-portfolio/src/components/testimonial/Testimonial.js
@@ -29,6 +29,38 @@ const Testimonial = () => {
           encouraged me to reach the next level. Every time I finish a class I
           feel taller and stronger and I thank my body for what it can do.
           Thanks to Dee at Yoga For Life!!!
+          <br></br>
+          <br></br>
+          Jane
+          <br></br>
+          <br></br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget
+          diam ut ipsum porta dignissim in non ex. Nam aliquet, arcu sit amet
+          pharetra tincidunt, mi turpis dignissim lorem, ac commodo mauris ante
+          at arcu. Mauris mattis risus sem, non finibus turpis iaculis rhoncus.
+          Vivamus porta efficitur mi in consequat. Class aptent taciti sociosqu
+          ad litora torquent per conubia nostra, per inceptos himenaeos. Proin
+          nec enim purus. Vestibulum pellentesque semper erat, vel vulputate ex
+          pretium a. Aliquam varius orci vitae neque pulvinar, id interdum metus
+          dictum. Etiam tempor, lorem sed sollicitudin imperdiet, felis diam
+          ultrices lacus, a consequat risus sem vulputate massa. Maecenas
+          dictum, augue ut euismod vulputate, velit mauris pharetra dui, sit
+          amet vestibulum leo risus a turpis.
+          <br></br>
+          <br></br>
+          Monica
+          <br></br>
+          <br></br>
+          In in vehicula magna. Sed blandit libero sit amet metus consequat
+          egestas. Sed sit amet ante magna. Duis luctus lorem id orci accumsan
+          luctus. Quisque diam nisi, feugiat et lectus vel, pretium blandit
+          ipsum. Vivamus lacus orci, interdum vel volutpat eget, luctus nec
+          elit. Sed vel lacus vel risus maximus facilisis et ac nulla. Donec
+          egestas tortor purus, vel dictum ex rhoncus maximus. Quisque tristique
+          odio sapien, eget commodo erat consectetur consectetur. Aenean at
+          dapibus dui. Donec eget augue metus. Vivamus tristique varius eros
+          feugiat hendrerit. Nam pretium congue condimentum. Duis ut nunc
+          egestas quam consequat commodo. Ut fermentum egestas egestas.
         </p>
       </section>
     </div>
